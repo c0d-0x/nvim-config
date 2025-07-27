5fdcacef16026c6d3d275c23d7ab5c08c52fb694
c0d-0x <c0d_0x007@proton.me>
2025-07-27 06:04:32 +0100

    cleanup and new_lsp config: no meson

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
-require "core.keymaps"
-require "core.options"
-require "core.snippets"
-require "core.lazy"
+require("core.keymaps")
+require("core.options")
+require("core.snippets")
+require("core.lazy")
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp", "java", "h", "cs" },
-  callback = function()
-    vim.bo.commentstring = "/* %s */"
-  end,
+	pattern = { "c", "cpp", "java", "h", "cs" },
+	callback = function()
+		vim.bo.commentstring = "/* %s */"
+	end,
 })
